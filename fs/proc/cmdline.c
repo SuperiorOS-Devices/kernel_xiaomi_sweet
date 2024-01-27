2b8bfa79e9df3ccd8268160dc6d22af7f201b13c
Pranav Vashi <neobuddy89@gmail.com>
2024-01-27 21:09:31 +0530

    proc: cmdline: Fix clang declaration error
    Change-Id: I2767de35f3dd81793eb898d34af0d9d65fd4ba21
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -35,8 +35,9 @@
 static char proc_command_line[COMMAND_LINE_SIZE];
 
 static void proc_command_line_init(void) {
-        char *offset_addr;
-
+#ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
+	char *offset_addr;
+#endif
 	strcpy(proc_command_line, saved_command_line);
 
 #ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
